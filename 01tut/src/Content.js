ac28aaeeba562dce46624fd48e1da9a60bd4b9a9
Aamirmuhammad91 <aamirmuhammad91@gmail.com>
2022-01-20 16:13:14 +0100

    Click Events

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -6,9 +6,23 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return name[int];
   };
+  const handleClick = () => {
+    console.log('you clicked it');
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} clicked it`);
+  };
+
+  const handleClick3 = (a) => {
+    console.log(a.target.innerHTML);
+  };
   return (
     <main>
       <p>my name is {handleNameChange()}</p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2('Dave')}>Click it</button>
+      <button onClick={(a) => handleClick3(a)}>Click it</button>
     </main>
   );
 };
